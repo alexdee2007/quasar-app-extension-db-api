4f911dd6d7210152535faefd85a05e483ef0587a
o.diachenko.2005 <o.diachenko.2005@in.np.gov.ua>
2020-05-12 14:30:54 +0300

    dicts

--- src/api/dict.js
+++ src/api/dict.js
@@ -1,17 +1,17 @@
 export default $axios => ({
 
-    total: async (name) => {
-      const {data} = await $axios.get(`/dictionaries/total?name=${name}`);
+    total: async (name, language = 'UK') => {
+      const {data} = await $axios.get(`/dictionaries/total?name=${name}&language=${language}`);
       return data;
     },
 
-    chunk: async (name, value) => {
-      const {data} = await $axios.get(`/dictionaries/chunk?name=${name}&value=${value}`);
+    chunk: async (name, language = 'UK', value = '') => {
+      const {data} = await $axios.get(`/dictionaries/chunk?name=${name}&language=${language}&value=${value}`);
       return data;
     },
 
-    dicts: async (dicts) => {
-      const {data} = await $axios.post(`/dictionaries/dicts`, {dicts});
+    review: async (name, language = 'UK', hash = '') => {
+      const {data} = await $axios.get(`/dictionaries/review?name=${name}&language=${language}&hash=${hash}`);
       return data;
     }
 
